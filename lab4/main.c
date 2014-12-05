171d269eb344c20ff72300eddcc049569772b829
LI-WEI TSENG <tsengliwei@gmail.com>
2014-12-04 18:16:23 -0800

    Initial Commit

---
+++ lab4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab4
+//
+//  Created by Li-Wei Tseng on 12/4/14.
+//  Copyright (c) 2014 Li-Wei Tseng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
